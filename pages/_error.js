ec0bec5528f8a8dd1700e752699855f76107cf99
jafar rezaei <bomber.man87@yahoo.com>
2019-08-03 16:49:41 +0430

    add new files

---
+++ pages/_error.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Error extends React.Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null
+    return { statusCode }
+  }
+
+  render() {
+    return (
+      <p>
+        {this.props.statusCode
+          ? `یک خط در سمت سرور رخ داده است : ${this.props.statusCode}`
+          : 'یک خطا در سمت مرورگر شما رخ داده است' }
+      </p>
+    )
+  }
+}
+
+export default Error
