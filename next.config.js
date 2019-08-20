3bb0d4a5570516fc348f249932f873396539e7cb
jafar rezaei <bomber.man87@yahoo.com>
2019-08-20 23:27:42 +0430

    add sass and saga , redux to project

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+const withCSS = require("@zeit/next-css");
+module.exports = withCSS(withSass({
+    target: 'serverless',
+    webpack (config, options) {
+        config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000
+                }
+            }
+        });
 
-module.exports = withCSS({
-  target: 'serverless',
-});
+        return config;
+    }
+}));
